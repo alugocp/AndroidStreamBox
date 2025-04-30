a39cd7ab13b38946e9ee19575760e870032916bb
Alex Lugo <alugocp@aim.com>
2025-04-29 23:52:21 -0400

    AndroidScreenCaster version

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # Pillbug
 This is a phone screen share hack I threw together for any server that runs Arch Linux (i.e. Raspberry Pi).
 I use it to replace my proprietary streaming box.
+It uses [AndroidScreenCaster](https://github.com/magicsih/AndroidScreenCaster).
 
 ## Helpful Arch commands
 Resize the root filesystem partition:
 ```bash
-mount -o remount,size=1G /run/archiso/cowspace
+mount -o remount,size=4G /run/archiso/cowspace
 ```
 
 View free space in Arch:
 ```bash
 df -h
+```
+
+Download a 9 second video of waves:
+```bash
+curl https://assets.mixkit.co/videos/5016/5016-720.mp4 -o waves.mp4
 ```
--- install.sh
+++ install.sh
@@ -1,34 +1,8 @@
 #!/bin/bash
-# You cannot run this script as the root user
-
-# 4 GB RAM
+# Requirements:
+# 1 GB RAM
 # 4 GB cowspace
 
 # Install all Pacman dependencies
 sudo pacman -Sy
-sudo pacman -S base-devel jre17-openjdk git gcc meson pkgconf debugedit fakeroot
-# patchutils clang nasm amf-headers ffnvcodec-headers opencl-headers vulkan-headers poppler
-
-# lcevcdec lensfun-git opencv2 openvino quirc chromaprint-fftw davs2 libaribcaption
-# libklvanc rockchip-mpp shine uavs3d-git vo-amrwbenc vvenc xavs xavs2 xevd xeve
-
-# Add alugocp user and add to sudoers file
-# yay-git AUR, use yay to install ffmpeg-full?
-
-# Install decklink-sdk
-git clone https://aur.archlinux.org/decklink-sdk.git
-cd decklink-sdk
-makepkg -si
-cd ..
-
-# Install ffmpeg-full
-git clone https://aur.archlinux.org/ffmpeg-full.git
-cd ffmpeg-full
-makepkg -si
-cd ..
-
-# Download and build the scrcpy repository
-git clone https://github.com/Genymobile/scrcpy.git
-cd scrcpy
-meson build
-ninja -C build
+sudo pacman -S ffmpeg
